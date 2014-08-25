5cd9c754c348b3e82c624898d71151330acd6bd9
k.tama <k.wireless.mail@gmail.com>
2014-08-25 22:50:13 +0900

    Singleton

---
+++ DesignPattern/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+	/// <summary>
+	/// シングルトンの実装
+	/// </summary>
+	class Singleton
+	{
+		// 唯一のインスタンス
+		private static Singleton singleton = new Singleton();
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		private Singleton()
+		{
+			// 別に何もしない
+		}
+
+		/// <summary>
+		/// 唯一のインスタンスを取得
+		/// </summary>
+		/// <returns></returns>
+		public static Singleton GetInstance()
+		{
+			return singleton;
+		}
+	}
+}
